aa5ad0812c2c15c01aab5953bc00da7c214887f6
Antoxerrr <a.malyarov99@gmail.com>
2021-07-07 23:12:27 +1000

    Анимация загрузки

--- illarinator/static/js/index.js
+++ illarinator/static/js/index.js
@@ -1,14 +1,44 @@
 const generateButton = document.getElementById('generate-button');
 const textHolder = document.getElementById('text-holder');
+let animationID = null;
 
 function capitalize(word) {
     return word[0].toUpperCase() + word.substr(1);
 }
 
+function buttonLoadingDots() {
+    const DOTS_LEN = 3;
+    generateButton.innerText += '.';
+    if (generateButton.innerText.length > DOTS_LEN) {
+        generateButton.innerText = '';
+    }
+}
+
+function startLoadingAnimation() {
+    generateButton.disabled = true;
+    generateButton.innerText = '';
+    generateButton.classList.add('loading');
+    animationID = setInterval(buttonLoadingDots, 250);
+}
+
+function endLoadingAnimation() {
+    generateButton.disabled = false;
+    clearInterval(animationID);
+    generateButton.classList.remove('loading');
+    generateButton.innerText = 'Замутить кликуху';
+}
+
 function generate() {
-    const response = axios.get('/generate')
-        .then(response => textHolder.innerText = capitalize(response.data))
-        .catch(error => textHolder.innerText = 'Что-то пошло не так :(');
+    startLoadingAnimation();
+    axios.get('/generate')
+        .then(response => {
+            textHolder.innerText = capitalize(response.data);
+            endLoadingAnimation();
+        })
+        .catch(error => {
+            textHolder.innerText = 'Что-то пошло не так :(';
+            endLoadingAnimation();
+        });
 }
 
 generateButton.onclick = generate;
